ba9958d272069d9626e14a23470a6dca69982f37
Zhiping Wu <zpwu@thoughtworks.com>
2017-01-28 11:20:14 +0800

    add norm cnn3 full2 model

--- tensorflow/car/model/model.py
+++ tensorflow/car/model/model.py
@@ -84,3 +84,32 @@ def cnn1_full1():
     model.compile(optimizer='adam', loss='mean_squared_error')
 
     return model
+
+def norm_cnn3_full1():
+    img_input = Input(shape=(120, 160, 3), name="img_input")
+
+    x = BatchNormalization()(img_input)
+    x = Convolution2D(8, 3, 3)(img_input)
+    x = Activation('relu')(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+
+    x = Convolution2D(16, 3, 3)(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+
+    x = Convolution2D(32, 3, 3)(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+
+    merged = Flatten()(x)
+
+    x = Dense(256)(merged)
+    x = Activation('linear')(x)
+    x = Dropout(.2)(x)
+
+    angle_out = Dense(1, name="angle_out")(x)
+
+    model = Model(input = [img_input], output = [angle_out])
+    model.compile(optimizer='adam', loss='mean_squared_error')
+
+    return model
