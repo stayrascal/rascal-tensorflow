1d4da5c8a7e104490f724d390b6b0b0feff2b272
Zhiping Wu <zpwu@thoughtworks.com>
2017-01-28 11:18:04 +0800

    add cnn1_full1 model

--- tensorflow/car/model/model.py
+++ tensorflow/car/model/model.py
@@ -64,3 +64,23 @@ def cnn3_full1_rnn1():
     model.compile(optimizer='adam', loss='mean_squared_error')
 
     return model
+
+def cnn1_full1():
+    img_input = Input(shape=(120, 160, 3), name="img_input")
+
+    x = Convolution2D(1, 3, 3)(img_input)
+    x = Activation('relu')(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+
+    merged = Flatten()(x)
+
+    x = Dense(32)(merged)
+    x = Activation('linear')(x)
+    x = Dropout(.05)(x)
+
+    angle_out = Dense(1, name="angle_out")(x)
+
+    model = Model(input = [img_input], output = [angle_out])
+    model.compile(optimizer='adam', loss='mean_squared_error')
+
+    return model
