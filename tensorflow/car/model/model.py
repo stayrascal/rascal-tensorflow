8d8f3fe5599345cf7e1f18205a26214877f9086f
Zhiping Wu <zpwu@thoughtworks.com>
2017-01-28 11:44:14 +0800

    add vision_2D function

--- tensorflow/car/model/model.py
+++ tensorflow/car/model/model.py
@@ -113,3 +113,45 @@ def norm_cnn3_full1():
     model.compile(optimizer='adam', loss='mean_squared_error')
 
     return model
+
+def vision_2D(dropout_frac=.2):
+    '''
+    Network with 4 convolutions, 2 residual shortcuts to predict angle.
+    '''
+    img_input = Input(shape=(120, 160, 3), name="img_input")
+
+    net = Convolution2D(64, 6, 6, subsample=(4, 4), name='conv0')(img_input)
+    net = dropout_frac(dropout_frac)(net)
+
+    net = Convolution2D(64, 3, 3, subsample=(2, 2), name='conv1')(net)
+    net = dropout_frac(dropout_frac)(net)
+
+    # Create residual to shortcut
+    aux1 = Flatten(name='aux1_flat')(net)
+    aux1 = Dense(64, name='aux1_dense')(aux1)
+
+    net = Convolution2D(128, 6, 6, subsample=(2, 2), border_mode='same', name='conv2')(net)
+    net = dropout_frac(dropout_frac)(net)
+
+    net = Convolution2D(128, 3, 3, subsample=(2, 2), border_mode='same', name='conv3')(net)
+    net = dropout_frac(dropout_frac)(net)
+
+    aux2 = Flatten(name='aux2_flat')(net)
+    aux2 = Dense(64, name='aux2_dense')(aux2)
+
+    net = Flatten(name='net_flat')(net)
+    net = Dense(512, activation='relu', name='net_dense1')(net)
+    net = Dropout(dropout_frac)(net)
+    net = Dense(256, activation='relu', name='net_dense2')(net)
+    net = Dropout(dropout_frac)(net)
+    net = Dense(128, activation='relu', name='net_dense3')(net)
+    net = Dropout(dropout_frac)(net)
+    net = Dense(64, activation='relu', name='net_dense4')(net)
+
+    # combine rsidual layers
+    net = merge([net, aux1, aux2], mode='sum') 
+    angle_out = Dense(1, name='angle_out')(net)
+    model = Model(input=[img_input], output=[angle_out])
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    return model
+
