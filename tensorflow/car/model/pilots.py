2481b12f4672f4bab7f2b9e467e60ba7f7987554
Zhiping Wu <zpwu@thoughtworks.com>
2017-01-28 17:42:07 +0800

    add pilots.py

---
+++ tensorflow/car/model/pilots.py
@@ -0,0 +1,50 @@
+import os
+import numpy as np
+import random
+
+from model
+
+class BasePilot():
+    '''
+    Base class to define common functions.
+    When creating a class, only override the funtions you'd like to replace.
+    '''
+
+    def decide(self, img_arr):
+        angle = 0
+        speed = 0
+
+        # Do prediction magic
+        return angle, speed
+
+class SwervePilot(BasePilot):
+    '''
+    Example predictor that should not be used.
+    '''
+
+    def __init__(self):
+        self.angle = random.randrange(-45, 46)
+        self.throttle = 20
+
+    def decide(self, img_arr):
+        new_angle = self.angle + random.randrange(-4, 5)
+        self.angle = min(max(-45, new_angle), 45)
+        return self, angle, self.throttle
+
+class KerasAngle():
+    def __init__(self, model, throttle):
+        self.model = model
+        self.throttle = throttle
+        self.last_angle = 0
+
+    def decide(self, img_arr):
+        img_arr = img_arr.reshape((1,) + img_arr.shape)
+        angle = self.model.predict(img_arr)
+        angle = angle[0][0]
+
+        # add some smoothing
+        a = .8
+        angle = a * angle + (1 - a) * self.last_angle
+        self.last_angle = angle
+
+        return angle, self.throttle
