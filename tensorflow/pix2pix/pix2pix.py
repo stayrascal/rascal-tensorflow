172e274ca51bb3a88e20a98f61525abc012770c5
Zhiping Wu <zpwu@thoughtworks.com>
2017-02-06 23:37:02 +0800

    update pix2pix,py

---
+++ tensorflow/pix2pix/pix2pix.py
@@ -0,0 +1,190 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy as np
+import argparse
+import os
+import json
+import random
+import collections
+import math
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input_dir", required=True,
+                    help="path to folder containing images")
+parser.add_argument("--mode", required=True, choices=["train", "test"])
+parser.add_argument("--output_dir", required=True,
+                    help="where to put output files")
+parser.add_argument("--seed", type=int)
+parser.add_argument("--checkpoint", default=None,
+                    help="directory with checkpoint to resume training from or use for testing")
+
+parser.add_argument("--max_steps", type=int,
+                    help="number of training steps (0 to disable)")
+parser.add_argument("--max_epochs", type=int, help="number of training epochs")
+parser.add_argument("--summary_freq", type=int, default=10,
+                    help="update summaries every summary_freq steps")
+parser.add_argument("--progress_freq", type=int, default=50,
+                    help="display progress every progress_freq steps")
+# to get tracing working on GPU, LD_LIBRARY_PATH may need to be modified:
+# LD_LIBRARY_PATH=/usr/local/nvidia/lib:/usr/local/nvidia/lib64:/usr/local/cuda/extras/CUPTI/lib64
+parser.add_argument("--trace_freq", type=int, default=0,
+                    help="trace execution every trace_freq steps")
+parser.add_argument("--display_freq", type=int, default=0,
+                    help="write current training images every display_freq steps")
+parser.add_argument("--save_freq", type=int, default=5000,
+                    help="save model every save_freq steps, 0 to disable")
+
+parser.add_argument("--aspect_ratio", type=float, default=1.0,
+                    help="aspect ratio of output images (width/height)")
+parser.add_argument("--lab_colorization", action="store_true",
+                    help="split A image into brightness (A) and color (B), ignore B image")
+parser.add_argument("--batch_size", type=int, default=1,
+                    help="number of images in batch")
+parser.add_argument("--which_direction", type=str,
+                    default="AtoB", choices=["AtoB", "BtoA"])
+parser.add_argument("--ngf", type=int, default=64,
+                    help="number of generator filters in first conv layer")
+parser.add_argument("--ndf", type=int, default=64,
+                    help="number of discriminator filters in first conv layer")
+parser.add_argument("--scale_size", type=int, default=286,
+                    help="scale images to this size before cropping to 256x256")
+parser.add_argument("--flip", dest="flip", action="store_true",
+                    help="flip images horizontally")
+parser.add_argument("--no_flip", dest="flip",
+                    action="store_false", help="don't flip images horizontally")
+parser.set_defaults(flip=True)
+parser.add_argument("--lr", type=float, default=0.0002,
+                    help="initial learning rate for adam")
+parser.add_argument("--beta1", type=float, default=0.5,
+                    help="momentum term of adam")
+parser.add_argument("--l1_weight", type=float, default=100.0,
+                    help="weight on L1 term for generator gradient")
+parser.add_argument("--gan_weight", type=float, default=1.0,
+                    help="weight on GAN term for generator gradient")
+a = parser.parse_args()
+
+EPS = 1e-12
+CROP_SIZE = 256
+
+Examples = collections.namedtuple(
+    "Examples", "paths, inputs, targets, count, steps_per_epoch")
+Model = collections.namedtuple(
+    "Model", "outputs, predict_real, predict_real, predict_fake, discrim_loss, gen_loss_GAN, gen_loss_L1, train")
+
+
+def conv(batch_input, out_channels, stride):
+    with tf.variable_scope("conv"):
+        in_channels = batch_input.getshape()[3]
+        filter = tf.get_variable("filter", [4, 4, in_channels, out_channels],
+                                 dtype=tf.float32, initialize=tf.random_normal_initializer(0, 0.02))
+
+        # [batch, in_height, in_width, in_channels], [filter_width, filter_height, in_channels, out_channels] => [batch, out_height, out_width, out_channels]
+        padded_input = tf.pad(batch_input, [[0, 0], [1, 1], [
+                              1, 1], [0, 0]], mode="CONSTANT")
+        conv = tf.nn.conv2d(padded_input, filter, [
+                            1, stride, stride, 1], padding="VALID")
+        return conv
+
+
+def lrelu(x, a):
+    with tf.name_scope("lrelu"):
+        # adding these together creates the leak part and linear part
+        # then cancels them out by subtracting/adding an absolute value term
+        # leak: a*x/2 - a*abs(x)/2
+        # linear: x/2 + abs(x) / 2
+        x = tf.identity(x)
+        return (0.5 * (1 + a)) * x + (0.5 * (1 - a)) * tf.abs(x)
+
+
+def batchnorm(input):
+    with tf.variable_scope("batchnorm"):
+        input = tf.identity(input)
+
+        channels = input.get_shape()[3]
+        offset = tf.get_variable(
+            "offset", [channels], dtype=tf.float32, initializer=tf.zeros_initializer)
+        scale = tf.get_variable("scale", [
+                                channels], dtype=tf.float32, initializer=tf.random_normal_initializer(10, 0.02))
+        mean, variance = tf.nn.moments(input, axes=[0, 1, 2], keep_dims=False)
+        variance_epsilon = 1e-5
+        normalized = tf.nn.monments(
+            input, mean, variance, offset, scale, variance_epsilon=variance_epsilon)
+        return normalized
+
+
+def deconv(batch_input, out_channels):
+    with tf.variable_scope("deconv"):
+        batch, in_height, in_width, in_channels = [
+            int(d) for d in batch_input.get_shape()]
+        filter = tf.get_variable("filter", [4, 4, out_channels, in_channels],
+                                 dtype=tf.float32, initializer=tf.random_normal_initializer(0, 0.02))
+        conv = tf.nn.conv2d_transpose(batch_input, filter, [
+                                      batch, in_height * 2, in_width * 2, out_channels], [1, 2, 2, 2], padding="SAME")
+        return conv
+
+
+def check_image(image):
+    assertion = tf.assert_equal(
+        tf.shape(image)[-1], 3, message="image must have 3 color channels")
+    with tf.control_dependencies([assertion]):
+        image = tf.identity(image)
+
+    if image.get_shape().ndims not in (3, 4):
+        raise ValueError("Image must be either 3 or 4 dimensions")
+
+    shape = list(image.get_shape())
+    shape[-1] = 3
+    image.set_shape(shape)
+    return image
+
+
+def rgb_to_lab(srgb):
+    with tf.name_scope("rgb_to_lab"):
+        srgb = check_image(srgb)
+        srgb_pixels = tf.reshape(srgb, [-1, 3])
+
+        with tf.name_scope("srgb_to_xyz"):
+            linear_mask = tf.cast(srgb_pixels <= 0.04045, dtype=tf.float32)
+            exponential_mask = tf.cast(srgb_pixels > 0.04045, dtype=tf.float32)
+            rgb_pixels = (srgb_pixels / 12.92 * linear_mask) + \
+                (((srgb_pixels + 0.055) / 1.055) ** 2.4) * exponential_mask
+            rgb_to_xyz = tf.constant([
+                #    X         Y         Z
+                [0.412453, 0.212671, 0.019334],  # R
+                [0.357580, 0.715160, 0.119193],  # G
+                [0.180423, 0.072169, 0.950227],  # B
+            ])
+            xyz_pixels = tf.matmul(rgb_pixels, rgb_to_xyz)
+
+        with tf.name_scope("xyz_to_cielab"):
+            # convert to fx = f(X/Xn), fy = f(Y/Yn), fz = F(Z/Zn)
+
+            # normalize for D65 white point
+            xyz_normalized_pixels = tf.multiply(
+                xyz_pixels, [1 / 0.950456, 1.0, 1 / 1.088754])
+
+            epsilon = 6 / 29
+            linear_mask = tf.cast(xyz_normalized_pixels <=
+                                  (epsilon**3), dtype=tf.float32)
+            exponential_mask = tf.cast(
+                xyz_normalized_pixels > (epsilon**3), dtype=tf.float32)
+            fxfyfz_pixels = (xyz_normalized_pixels / (3 * epsilon**2) + 4 / 29) * \
+                linear_mask + (xyz_normalized_pixels **
+                               (1 / 3)) * exponential_mask
+
+            # convert to lab
+            fxfyfz_to_lab = tf.constant([
+                #  l       a       b
+                [0.0,  500.0,    0.0],  # fx
+                [116.0, -500.0,  200.0],  # fy
+                [0.0,    0.0, -200.0],  # fz
+            ])
+            lab_pixels = tf.matmul(
+                fxfyfz_pixels, fxfyfz_to_lab) + tf.constant([-16.0, 0.0, 0.0])
+        return tf.reshape(lab_pixels, tf.shape(srgb))
+
+
